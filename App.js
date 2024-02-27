ce8a49093cad17c260c180ef23cc009efd4cee66
kessianaty <skessia43@gmail.com>
2024-02-27 18:30:05 -0300

    api commit

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+
+const request = async(callback) =>{
+  const response = await fetch('https://swapi.dev/api/people');
+  const parsed = await response.json();
+  callback(parsed.results);
+}
 
 export default function App() {
+  
+  const [registros, setRegistos] = useState([]);
+  useEffect(()=>{
+    request(setRegistos);
+  },[])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>Api do StarWars</Text>
+      <FlatList 
+      data={registros}
+      renderItem={({item})=>
+      <Text style={styles.itens}>
+        <Text>Nome: {item.name}{'\n'}</Text>
+        <Text>Peso: {item.mass}</Text>
+      </Text>
+    }
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#778899',
+    justifyContent: 'space-between',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 25,
+    paddingBottom:25,
   },
+  itens:{
+    flex: 1,
+    backgroundColor: '#000080',
+    marginBottom: 10,
+    marginRight: 10, 
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 10,
+    paddingBottom:10,
+    textAlign: 'center',
+    paddingVertical: 10,
+    color: '#fff',
+    fontSize: 20
+  },
+  titulo:{
+    fontSize: 30,
+    textAlign: 'center',
+    marginVertical:40
+  }
 });
